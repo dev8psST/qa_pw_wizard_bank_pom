13ae2be0181f6bb7544a550c59aca74903ada79a
dev8psST <mailfreesw@protonmail.com>
2025-04-04 06:11:39 -0400

    add some methods to AddCustomersPage.js

--- src/pages/manager/OpenAccountPage.js
+++ src/pages/manager/OpenAccountPage.js
@@ -1,9 +1,14 @@
 const { expect } = require('@playwright/test');
 
 export class AddCustomerPage {
-  constructor(page) {
+  constructor(page, firstName, lastName, postCode) {
     this.page = page; 
     this.currencyDropDown = page.locator('#currency');
+    this.customerDropDown = page.locator('#userSelect');
+    this.customerName = `${firstName} ${lastName}`;
+    this.btnProcess = page.getByRole('button', { name: 'Process' });
+    this.btnCustomers = page.getByRole('button', { name: 'Customers' });
+    this.myLocator = page.getByRole('table').getByRole('rowgroup').nth(1).getByRole('row').last();
   }
 
   async open() {
@@ -13,6 +18,14 @@ export class AddCustomerPage {
 
   async selectCurrency(currencyName) {
     await this.currencyDropDown.selectOption(currencyName);
+  }
+
+  async selectCustomer() {
+    await this.customerDropDown.selectOption(this.customerName);
+  }
+
+  async clickProcessBtn(){
+    await this.btnProcess.click();
   }
 
   async assertSelectCurrencyDropdownContainsValue(value) {
